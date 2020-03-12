39887e12ba72bffdb039e330bb29d67a031a4ffb
Matt Hartnett <matthew.e.hartnett@gmail.com>
2020-03-12 08:04:00 -0600

    Finished homework 8.

--- hw8.py
+++ hw8.py
@@ -1,3 +1,4 @@
+# I was unable to complete this assignment, because I could not add the necessary constraints into z3
 from z3 import*
 def set_cover(Ca):
     x = []
@@ -23,15 +24,3 @@ def set_cover(Ca):
 if __name__ == '__main__':
     Ca = [{0,10}, {0,1,4}, {1,2,4,5,6,7}, {0,1,3,5,9}, {0,3}, {2,6,8,11}, {2,7,8,10}, {3,9}]
     print(set_cover(Ca))
-# x0 = Bool('x0')
-# x1 = Bool('x1')
-# x2 = Bool('x2')
-# x3 = Bool('x3')
-# x4 = Bool('x4')
-# x5 = Bool('x5')
-# x6 = Bool('x6')
-# x7 = Bool('x7')
-# x8 = Bool('x8')
-# x9 = Bool('x9')
-# x10 = Bool('x10')
-# x11 = Bool('x11')
