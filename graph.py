32d9ff74b9694f3d45e6c7caa9402941cb8b3ef4
HartnettMatt <matthew.e.hartnett@gmail.com>
2020-04-20 09:39:51 -0600

    Wrote first version of dijkstra, still need to do rigerous testing

--- graph.py
+++ graph.py
@@ -2,6 +2,8 @@ class Vertex:
     def __init__(self, node):
         self.id = node
         self.adjacent = {}
+        self.visited = False
+        self.distance = 0
 
     def __str__(self):
         return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
@@ -50,6 +52,33 @@ class Graph(object):
     def get_vertices(self):
         return self.vert_dict.keys()
 
+    def dijkstra(self, frm, to):
+        if to not in self.vert_dict:
+            print("error: starting point not found")
+            return None
+        if frm not in self.vert_dict:
+            print("error: destination not found")
+            return None
+        self.vert_dict[frm].visited = True
+        self.vert_dict[frm].distance = 0
+        solvedList = [self.vert_dict[frm]]
+        temp = Vertex('temp')
+        temp = self.vert_dict[frm]
+        solvedV = Vertex('solved')
+        while self.vert_dict[to].visited != True:
+            minDist = 2147483647
+            for x in range(len(solvedList)):
+                temp = solvedList[x]
+                for i in temp.adjacent:
+                    if i.visited == False:
+                        dist = temp.distance + temp.get_weight(i)
+                        if dist < minDist:
+                            minDist = dist
+                            solvedV = i
+            solvedV.distance = minDist
+            solvedV.visited = True
+            solvedList.append(solvedV)
+        return self.vert_dict[to].distance
 
 graph = Graph()
 
@@ -59,3 +88,4 @@ graph.add_edge('a','b',9)
 graph.get_vertices
 print(graph.get_vertices())
 print(graph.get_vertex('a'))
+print(graph.dijkstra('a','b'))
