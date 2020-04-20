170b70f30cbde3d1a8711f29b3cb5ecc245e73ee
HartnettMatt <matthew.e.hartnett@gmail.com>
2020-04-20 15:24:01 -0600

    Tested Dijkstra more throughly, fixed a few bugs, and added comments

--- graph.py
+++ graph.py
@@ -1,37 +1,39 @@
 class Vertex:
-    def __init__(self, node):
-        self.id = node
+    def __init__(self, name):
+        self.id = name
         self.adjacent = {}
         self.visited = False
+        # Distance is a temporary to keep track in Dijkstra
         self.distance = 0
 
     def __str__(self):
         return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
 
-    def add_neighbour(self, neighbour, weight = 0):
-        self.adjacent[neighbour] = weight
+    def add_adjacent(self, adjacent, weight = 0):
+        self.adjacent[adjacent] = weight
 
-    def get_connections(self):
+    def get_adjacent(self):
         return self.adjacent.keys()
 
     def get_id(self):
         return self.id
 
-    def get_weight(self, neighbour):
-        return self.adjacent[neighbour]
+    def get_weight(self, adjacent):
+        return self.adjacent[adjacent]
 
 class Graph(object):
     def __init__(self):
+        # vert_dict is the dictionary of vertices
         self.vert_dict = {}
         self.num_vertices = 0
 
     def __iter__(self):
         return iter(self.vert_dict_values())
 
-    def add_vertex(self, node):
+    def add_vertex(self, name):
         self.num_vertices += 1
-        new_vertex = Vertex(node)
-        self.vert_dict[node] = new_vertex
+        new_vertex = Vertex(name)
+        self.vert_dict[name] = new_vertex
         return new_vertex
 
     def get_vertex(self, n):
@@ -40,35 +42,53 @@ class Graph(object):
         else:
             return None
 
+# Add weighted edge to the graph, will create vertexes if they don't already exist
     def add_edge(self, frm, to, weight = 0):
         if frm not in self.vert_dict:
             self.add_vertex(frm)
         if to not in self.vert_dict:
             self.add_vertex(to)
 
-        self.vert_dict[frm].add_neighbour(self.vert_dict[to], weight)
-        self.vert_dict[to].add_neighbour(self.vert_dict[frm], weight)
+        self.vert_dict[frm].add_adjacent(self.vert_dict[to], weight)
+        self.vert_dict[to].add_adjacent(self.vert_dict[frm], weight)
 
     def get_vertices(self):
         return self.vert_dict.keys()
 
+# Set all distances to zero and all vertexes to unvisited
+    def reset_graph(self):
+        for k in self.vert_dict:
+            self.vert_dict[k].distance = 0
+            self.vert_dict[k].visited = False
+
+# Find the fastest path from two points in a weighted graph
     def dijkstra(self, frm, to):
+        # Make sure that both points are in the graph
         if to not in self.vert_dict:
-            print("error: starting point not found")
-            return None
+            print("error: destination point not found")
+            return -1
         if frm not in self.vert_dict:
-            print("error: destination not found")
-            return None
+            print("error: starting point not found")
+            return -1
+
+        # Intialize starting values:
         self.vert_dict[frm].visited = True
         self.vert_dict[frm].distance = 0
-        solvedList = [self.vert_dict[frm]]
+        solvedList = []
+        solvedList.append(self.vert_dict[frm])
+
+        # Temp keeps track of which vertex is being tested
         temp = Vertex('temp')
         temp = self.vert_dict[frm]
+
+        # solvedV keeps track of which vertex is closest for that iteration
         solvedV = Vertex('solved')
+
+        # Loop until the destination is reached
         while self.vert_dict[to].visited != True:
             minDist = 2147483647
-            for x in range(len(solvedList)):
-                temp = solvedList[x]
+            for x in solvedList:
+                temp = x
                 for i in temp.adjacent:
                     if i.visited == False:
                         dist = temp.distance + temp.get_weight(i)
@@ -78,14 +98,8 @@ class Graph(object):
             solvedV.distance = minDist
             solvedV.visited = True
             solvedList.append(solvedV)
-        return self.vert_dict[to].distance
 
-graph = Graph()
-
-graph.add_vertex('a')
-graph.add_vertex('b')
-graph.add_edge('a','b',9)
-graph.get_vertices
-print(graph.get_vertices())
-print(graph.get_vertex('a'))
-print(graph.dijkstra('a','b'))
+        # Store actual distance, reset the graph, and return:
+        distance = self.vert_dict[to].distance
+        self.reset_graph()
+        return distance
---
+++ graphTest.py
@@ -0,0 +1,38 @@
+# Graph Test Code:
+from graph import *
+
+graph = Graph()
+
+# Build a graph of various connections with various weights for testing purposes
+graph.add_vertex('a')
+graph.add_vertex('b')
+graph.add_vertex('c')
+graph.add_vertex('d')
+graph.add_vertex('e')
+graph.add_vertex('f')
+graph.add_vertex('g')
+
+graph.add_edge('a','b',2)
+graph.add_edge('a','c',1)
+graph.add_edge('a','d',3)
+graph.add_edge('b','d',1)
+graph.add_edge('b','e',7)
+graph.add_edge('c','d',1)
+graph.add_edge('d','f',1)
+graph.add_edge('d','e',10)
+graph.add_edge('f','g',1)
+graph.add_edge('g','e',4)
+
+# Testing of a few graph functions:
+print(graph.get_vertices())
+print(graph.get_vertex('a'))
+
+# Testing DijkstraAlgorithm:
+print("\nTesting single connection: \nExpected output = 2")
+print("Dijkstra Output = " + str(graph.dijkstra('a','b')))
+
+print("\nTesting 3 possible routes: \nExpected output = 2")
+print("Dijkstra Output = " + str(graph.dijkstra('a','d')))
+
+print("\nTesting 7 possible routes: \nExpected output = 8")
+print("Dijkstra Output = " + str(graph.dijkstra('a','e')))
